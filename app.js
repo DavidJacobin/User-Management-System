eed3742888e9c7addc6c63ca5f2d9092c9b64b06
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2022-12-30 20:06:22 +0200

    connection pool added

--- app.js
+++ app.js
@@ -22,6 +22,22 @@ app.engine('hbs', exphbs.engine({ extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
 
+//connection pool
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+//connect to db
+pool.getConnection((err,connection) =>{
+    if(err) throw err;
+    console.log('Connected to DB');
+})
+
+
 //router
 app.get('/', (req, res) =>{
     res.render('home')
