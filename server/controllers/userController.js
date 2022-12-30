cce4cbc14e12cc5a3a7ae0b553ace59084489b14
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2022-12-30 20:55:45 +0200

    connected DB to views/home and display info from MySQL DB

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,33 @@
 
-exports.view = (req, res) =>{
-    res.render('home')
+const mysql = require('mysql')
+
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+
+exports.view = (req, res) => {
+
+
+
+    //connect to db
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        console.log('Connected to DB');
+
+
+        connection.query('SELECT * FROM user', (err, rows) => {
+            connection.release();
+
+            if (!err) {
+                res.render('home', { rows })
+            } else {
+                console.log(err);
+            }
+        })
+    })
 };
