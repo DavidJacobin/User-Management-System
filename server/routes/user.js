153e0bb78b44c2a1d2949f1b408adda84b38f9d8
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-01-02 20:28:56 +0200

    final correction

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,12 +8,9 @@ const userController = require('../controllers/userController')
 
 router.get('/', userController.view);
 router.post("/", userController.find);
-
-router.get("/:id", userController.delete);
-
-router.get("/adduser", userController.create);
 router.post("/adduser", userController.form);
-
+router.get("/adduser", userController.create);
+router.get("/:id", userController.delete);
 router.get("/edituser/:id", userController.edit);
 router.post("/edituser/:id", userController.edited);
 
